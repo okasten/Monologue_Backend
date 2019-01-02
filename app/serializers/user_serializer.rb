9e7e839e8b37f64233c1922bf279ae6080b0096c
github username <github email address>
2019-01-02 16:34:04 -0500

    styled

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :password, :email
+  attributes :id, :name, :username, :password, :email, :picture
   has_many :usermonologues
   has_many :monologues, through: :usermonologues
 end
